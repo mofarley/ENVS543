b51d56b312dfaa45140edb3115b554a607be6a0b
Moses Farley <mcfarley@email.wm.edu>
2024-11-09 20:26:09 -0500

    annoying raster stuff

--- rasterscript.R
+++ rasterscript.R
@@ -1,17 +1,8 @@
----
-title: "RasterHW"
-author: "Moses Farley"
-format: html
-editor: visual
----
-
-```{r}
-#| echo: false
 library(tidyverse)
 library(raster)
 library(sf)
 raster_url <- "https://github.com/DyerlabTeaching/Raster-Data/raw/main/data/alt_22.tif"
-beetle_url <- "https://raw.githubusercontent.com/DyerlabTeaching/Raster-Data/main/data/AraptusDispersalBias.csv"
+beetle_url <- "https://raw.githubuserconteendnt.com/DyerlabTeaching/Raster-Data/main/data/AraptusDispersalBias.csv"
 r <- raster(raster_url)
 read_csv( beetle_url ) %>% 
   st_as_sf( coords=c("Longitude","Latitude"), crs=4326 ) -> beetles
@@ -21,6 +12,7 @@ cropped_r <- crop(r, baja_extent)
 rm(r)
 plot( cropped_r )
 plot( beetles, add=TRUE, col="purple", pch=16, cex=1.5)
+text(st_coordinates(beetles), labels = beetles$Site, pos = 3, cex = 0.8, col = "black")
 click( cropped_r, xy=TRUE, 
        n=3 ) -> points
-```
+
